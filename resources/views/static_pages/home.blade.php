a4f03d1da1e5e4c7b099e3d1dce4c2e9cecbb7d3
hujianhua0830 <345914018@qq.com>
2021-11-06 18:04:03 +0800

    初始化样式

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello Friends</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">老胡第一个项目</a> 的主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
+@stop
